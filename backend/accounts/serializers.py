8ac49aa372af3756cd063849dd0aeef9ec2214d8
Atifulhaq <Atif@gmail.com>
2025-11-14 23:32:44 +0000

    first commit

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -16,3 +16,5 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+
